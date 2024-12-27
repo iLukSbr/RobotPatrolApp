b3935373886b474c1e05479f50bc082ed9ef4278
Letícia Walger Amaro <leticiaamaro@alunos.utfpr.edu.br>
2024-12-27 16:29:16 -0300

    All fields get data from server

--- app/src/main/java/com/example/robotpatrolapp/MainActivity.kt
+++ app/src/main/java/com/example/robotpatrolapp/MainActivity.kt
@@ -13,7 +13,11 @@ import java.io.InputStream
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var tvCarbonDioxide: TextView
+    private lateinit var tvTemperature: TextView
+    private lateinit var tvHumidity: TextView
     private lateinit var tvAmmonia: TextView
+    private lateinit var tvFlame: TextView
     private var isReceiving = true // Control flag to stop receiving when needed
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,7 +30,11 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        tvCarbonDioxide = findViewById(R.id.tvCarbonDioxideValue)
+        tvTemperature = findViewById(R.id.tvTempValue)
+        tvHumidity = findViewById(R.id.tvHumidityValue)
         tvAmmonia = findViewById(R.id.tvAmmoniaValue)
+        tvFlame = findViewById(R.id.tvFlameValue)
 
         CoroutineScope(Dispatchers.IO).launch {
             receiveData()
@@ -47,7 +55,11 @@ class MainActivity : AppCompatActivity() {
         val socket = SocketManager.socket
         if (socket == null) {
             withContext(Dispatchers.Main) {
+                tvCarbonDioxide.text = "0"
+                tvTemperature.text = "0"
+                tvHumidity.text = "0"
                 tvAmmonia.text = "0"
+                tvFlame.text = "0"
             }
             return
         }
@@ -63,15 +75,26 @@ class MainActivity : AppCompatActivity() {
                     if (bytesRead > 0) {
                         val receivedMessage = String(buffer, 0, bytesRead)
 
+                        // Separates message based on ','
+                        val parts = receivedMessage.split(",")
+
                         withContext(Dispatchers.Main) {
-                            tvAmmonia.text = receivedMessage
+                            tvCarbonDioxide.text = parts.getOrNull(0) ?: "NaN"
+                            tvTemperature.text = parts.getOrNull(1) ?: "NaN"
+                            tvHumidity.text = parts.getOrNull(2) ?: "NaN"
+                            tvAmmonia.text = parts.getOrNull(3) ?: "NaN"
+                            tvFlame.text = parts.getOrNull(4) ?: "NaN"
                         }
                     }
                 }
             } catch (e: Exception) {
                 e.printStackTrace()
                 withContext(Dispatchers.Main) {
+                    tvCarbonDioxide.text = "NaN"
+                    tvTemperature.text = "NaN"
+                    tvHumidity.text = "NaN"
                     tvAmmonia.text = "NaN"
+                    tvFlame.text = "NaN"
                 }
             }
         }
